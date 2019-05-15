69525f23dd8149bdcb0f769c8534e42aae89c56e
Leonardo Machado Leite <38584731+LeonardoMachadoLeite@users.noreply.github.com>
2019-05-15 10:23:18 -0300

    Adicionando os atributos getters and setters 10:23

--- src/main/java/model/Curso.java
+++ src/main/java/model/Curso.java
@@ -1,7 +1,20 @@
 package model;
 
+import java.util.UUID;
+
 public class Curso {
 
 int CodigoCurso;
+private String NomeCurso;
 
+String profID = UUID.randomUUID().toString();
+
+
+    public String getNomeCurso() {
+        return NomeCurso;
+    }
+
+    public void setNomeCurso(String nomeCurso) {
+        NomeCurso = nomeCurso;
+    }
 }
