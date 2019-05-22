a69592ae62cce472cc06a8c2be7e84db397b00a0
erivaschaves <erivas_12@hotmail.com>
2019-05-22 16:08:27 -0300

    adição da classe histórico

---
+++ src/main/java/model/Historico.java
@@ -0,0 +1,64 @@
+
+package javaapplication58;
+
+
+public class Historico {
+    private int ano;
+    private int semestre;
+    //private Matricula matriculaAluno;
+    private String situacao;
+    private double media;
+    private int faltas;
+    
+    
+    public void verificarSituacaoAluno(){
+        
+    }
+    public void verificarMedia(){
+        
+    }
+    public void verificarFaltas(){
+        
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public int getSemestre() {
+        return semestre;
+    }
+
+    public void setSemestre(int semestre) {
+        this.semestre = semestre;
+    }
+
+    public String getSituacao() {
+        return situacao;
+    }
+
+    public void setSituacao(String situacao) {
+        this.situacao = situacao;
+    }
+
+    public double getMedia() {
+        return media;
+    }
+
+    public void setMedia(double media) {
+        this.media = media;
+    }
+
+    public int getFaltas() {
+        return faltas;
+    }
+
+    public void setFaltas(int faltas) {
+        this.faltas = faltas;
+    }
+    
+ }
