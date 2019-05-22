749600809c3e919e899ad55059e1b2c7df8e7b1d
Vitor Giovanny <vitorgato9@hotmail.com>
2019-05-22 17:07:10 -0300

    Resolvido Class Curso
    Resolvido

--- src/main/java/model/Curso.java
+++ src/main/java/model/Curso.java
@@ -8,46 +8,28 @@ public class Curso{
 
 //Atributos
 private UUID CodigoCurso;
-private int totalCredito;
+private final int totalCredito;
 private String NomeCurso;
 
 //ArrayList
-private ArrayList<Curso> Cursos = new ArrayList<Curso>();
+private ArrayList<Matriz> matrizes;
+private ArrayList<Professor> prof;
 
-//Objetos
-Professor prof ;
 
 //Construtores
 public Curso(){
+    matrizes = new ArrayList<>();
+    prof = new ArrayList<>();
+    
+    this.totalCredito = 27;
     this.CodigoCurso = UUID.randomUUID();
-    prof = new Professor();
 }
 
-
 int visualizarCreditosCurso(){
-    this.setTotalCredito(27);
     return this.getTotalCredito();
 }
 
-
-public String listarProfessorCurso(){
-    return prof.getNomeProfessor();
-}
-
-public void adicionarCurso(String Ncurso){
-    this.setNomeCurso(Ncurso);
-    
-}
-
-ArrayList<Curso> ListarCurso(){
-    for(Curso s : getCursos()){
-        System.out.println(s);
-    }
-    return this.getCursos();
-}
-
-
-//Getters and Setters
+//////////////////GETTERS AND SETTERS///////////////////////////
 
     public String getNomeCurso() {
         return NomeCurso;
@@ -57,30 +39,27 @@ ArrayList<Curso> ListarCurso(){
         NomeCurso = nomeCurso;
     }
 
-
-
     public int getTotalCredito() {
         return totalCredito;
-    }
-
-    public void setTotalCredito(int totalCredito) {
-        this.totalCredito = totalCredito;
     }
 
     public UUID getCodigoCurso() {
         return CodigoCurso;
     }
 
-
-
-
-    public ArrayList<Curso> getCursos() {
-        return Cursos;
+    public void addMatriz(Matriz matrizes) {
+        this.getMatrizes().add(matrizes);
     }
 
-
-    public void setCursos(ArrayList<Curso> Cursos) {
-        this.Cursos = Cursos;
+    public ArrayList<Matriz> getMatrizes() {
+        return matrizes;
     }
 
+    public ArrayList<Professor> getProf() {
+        return prof;
+    }
+
+    public void addProf(Professor prof) {
+        this.getProf().add(prof);
+    }
 }
