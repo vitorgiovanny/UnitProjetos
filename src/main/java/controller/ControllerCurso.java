5039b77044095f5be9f353ad79f4931feec272c8
vitorgiovanny <vitorgato9@hotmail.com>
2019-06-05 15:11:58 -0300

    ENVIANDO CONTROLLER CURSO

--- src/main/java/controller/ControllerCurso.java
+++ src/main/java/controller/ControllerCurso.java
@@ -1,6 +1,33 @@
-package controller;
+package javaapplication1.controller;
+
+import controller.Controller;
+import java.util.ArrayList;
+import model.Curso;
 
 public class ControllerCurso {
 
     private Controller controllerGeral;
+    private ArrayList<Curso> Curso;
+    
+    public ControllerCurso() {
+        this.Curso = new ArrayList<Curso>();
+    }
+
+    public Controller getControllerGeral() {
+        return controllerGeral;
+    }
+
+    public void setControllerGeral(Controller controllerGeral) {
+        this.controllerGeral = controllerGeral;
+    }
+    
+
+    
+    public ArrayList<Curso> getprofessor() {
+        return Curso;
+    }
+    
+    public void AddProfessor(Curso curso) {
+        this.Curso.add(curso);
+    }
 }
