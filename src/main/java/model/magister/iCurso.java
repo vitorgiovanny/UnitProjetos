ba80b626bb362178dd2fb180bede8f8a20708fff
vitorgiovanny <vitorgato9@hotmail.com>
2020-11-02 18:53:44 -0300

    Interfaces iCurso e iPessoa, com objetivo de modelar os dados padrão

--- src/main/java/model/magister/iCurso.java
+++ src/main/java/model/magister/iCurso.java
@@ -4,7 +4,7 @@ package model.magister;
 import model.Disciplina;
 
 public interface iCurso {
-    public Disciplina handler(Disciplina disciplina);
+    
     public void NomeCurso(String nNome);
     public void InsertCodigoCurso(int codigo);
 }
---
+++ src/main/java/model/magister/iPessoa.java
@@ -0,0 +1,10 @@
+
+package model.magister;
+
+public interface iPessoa {
+    
+    public void setNome(String nome);
+    public void setCpf(String cpf);
+    public String getNome();
+    public int getIdade();
+}
